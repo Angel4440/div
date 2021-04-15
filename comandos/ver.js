406b254ef4e9255e75d2bc0d4a531501ac4792ca
Angel4440 <71406660+Angel4440@users.noreply.github.com>
2021-04-14 21:57:18 -0300

    ver.js

--- comandos/ver.js
+++ comandos/ver.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 module.exports.run = async (client, message, args) => {
   message.delete();
-    if ((message.author.id !== "817160910871330836"))
+    if ((message.author.id !== "767169424503078923"))
       return message.reply("Você Não Tem Permissão Para Isso!");
   let servidores = client.guilds.size;
   let usuarios = client.users.size;
@@ -12,7 +12,7 @@ module.exports.run = async (client, message, args) => {
   let invisible = client.users.filter(m => m.presence.status === "offline");
 
   const embed = new Discord.RichEmbed()
-    .setTitle(`**D X T Divs!**`)
+    .setTitle(`**Fml Divs!**`)
     .setDescription(
       `**Servidores:** ${client.guilds.size}\n**Total:** ${client.users.size}\n\n**Onlines:** ${on.size}\n**Ausentes:** ${ausente.size}\n**Ocupados:** ${npertube.size}\n**Invisíveis:** ${invisible.size}`
     )
